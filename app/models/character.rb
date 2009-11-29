a0903c150878739c712f9d5bdac484bde68d1718
Luke Mcildoon <luke@twofiftyfive.net>
2009-11-30 03:15:42 +1100

    more character stuff disassembled

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,12 +8,8 @@ class Character < BaseData
     [:stats,            Stats],
     [:base_stats_maybe, Stats],
     [:unknown02,        [:int32,8]],
-    [:mana,             :uint32]
+    [:mana,             :uint32],
+    [:unknown03,        [:int8,456]]
   )
-  
-  def disassemble(file)
-    returning super(file) do
-      (SIZE - 4).times { file.read(1) }
-    end
-  end
+
 end
---
+++ spec/models/characters_spec.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Characters do
+  
+  before do
+    @file = File.open(RAILS_ROOT+'/spec/fixtures/full_save.bin')
+    @characters = Characters.new
+  end
+  
+  def disassembled
+    @disassembled ||= @characters.disassemble(@file)
+    @disassembled
+  end
+  
+  it "should disassemble" do
+    disassembled.should have(16).entries
+    disassembled.first.should be_an_instance_of Character
+  end
+  
+  #lets just check 1st/2nd character's experience as a sanity check here
+  it "should disassemble correctly" do
+    disassembled.first.experience.value.should == 1469552283
+    disassembled.second.experience.value.should == 978368200
+  end
+  
+end
