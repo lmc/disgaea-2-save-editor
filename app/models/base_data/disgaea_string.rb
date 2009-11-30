b97ab815ae1d5ba912db818d492467f9c72b381a
Luke Mcildoon <luke@twofiftyfive.net>
2009-12-01 01:28:11 +1100

    disgaea string class

---
+++ app/models/base_data/disgaea_string.rb
@@ -0,0 +1,31 @@
+#Some strings are stored as plain-text, others are in this weird offset form @__@
+class BaseData::DisgaeaString < BaseData::String
+  TRANSLATIONS = {
+    64  => ' ',
+    70  => ':'
+  }
+  
+  def self.translate_char(disgaea_char)
+    case disgaea_char[0].ord #first character's numeral
+    when 130 #0x82
+      chr = disgaea_char[1]
+      chr -= 1 if chr > 128
+      (chr - 31).chr
+    when 129
+      TRANSLATIONS[disgaea_char[1]] || disgaea_char
+    else
+      "\000"
+    end
+  end
+  
+  def self.translate(disgaea_string)
+    disgaea_string.bytes.to_a.map(&:chr).in_groups_of(2).map do |*int16|
+      translate_char(int16.join)
+    end.join
+  end
+  
+  def disassemble(file_data)
+    super
+    self.value = self.class.translate(self.value)
+  end
+end
--- app/models/base_data/unknown_collection.rb
+++ app/models/base_data/unknown_collection.rb
@@ -1,7 +1,25 @@
 #A big fuck-it bucket to stop 2000-entry long arrays from flooding your terminal
 class BaseData::UnknownCollection < BaseData::StringCollection
+  undef value #HACK
   
   def inspect_value
     "size=#{size}"
   end
+  
+  def as_string
+    strings.map(&:value).map(&:chr)
+  end
+  
+  def as_int8
+    strings.map(&:value)
+  end
+  
+  def as_int16(offset = 0)
+    
+  end
+  
+  def as_int32(offset = 0)
+    
+  end
+  
 end
