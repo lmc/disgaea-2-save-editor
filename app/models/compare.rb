0c57795391e853981069de09cf3d52caedceb54a
Luke Mcildoon <luke@miko-miko.local>
2009-12-04 10:48:31 +1100

    awesome compare class

--- app/models/compare.rb
+++ app/models/compare.rb
@@ -1,49 +1,62 @@
 #TODO: Make this into an instance-able class, with accessor blocks for skipping, highlighting, sorting, etc.
+
+#To compare 3 character's tunknown blocks as int16s
+#  chars = Save.new.disassemble.characters
+#  comp = Compare.new(chars[0],chars[1],chars[2])
+#  comp.map = [:tunknown,:as_int16]
+#  comp.compare_string
 class Compare
+  attr_accessor :to_compare
+  attr_accessor :sorter
+  attr_accessor :map
+  attr_accessor :skip_if
+  attr_accessor :highlight_if
+  attr_accessor :display
   
-  VALUEIZER = proc { |obj| obj.respond_to?(:value) ? obj.value : obj }
+  FALSE_PROC = proc { |idx,*args| false }
+  DISPLAY_PROC = proc { |idx,*args| [idx,args].flatten.join("\t") }
   
-  def self.comp(values1,values2,options = {},&block)
-    values1 = values1.map(&VALUEIZER) if values1.respond_to?(:map)
-    values2 = values2.map(&VALUEIZER) if values2.respond_to?(:map)
-    block ||= proc { |u1,u2,idx| "#{idx}:\t#{u1}\t#{u2}" }
+  def initialize(*to_compare)
+    self.to_compare = [to_compare].flatten
+    self.map = []
     
-    options.reverse_merge!(
-      :skip_unless => proc { |u1,u2,idx| true }
-    )
-    
-    [values1.size,values2.size].max.times do |line|
-      args = [values1[line],values2[line],line]
-      next unless options[:skip_unless].call(*args)
-      puts block.call(*args)
-    end
+    self.map += [:tunknown,:as_int16]
   end
   
-  #s = Save.new.disassemble.characters
-  #compare two character's stats (at indexes 0 and 1)
-  #Compare.comp_array(s,0,1,:map => [:stats,:comp_values]) { |(k,u1),(k,u2)| "#{u1}\t#{k}\t#{u2}" }
-  #compare two character's unknown03 blocks
-  #Compare.comp_array(.,0,1,:map => [:unknown03])
-  #find interesting values
-  #Compare.comp_array(s,0,1,:map => [:unknown02],:skip_unless => proc {|u1,u2,idx| (u1 > 0 || u2 > 0) })
-  #find interesting differences
-  #Compare.comp_array(s,0,1,:map => [:unknown01],:skip_unless => proc {|u1,u2,idx| (u1 > 0 || u2 > 0) && (u1 != u2) })
-  def self.comp_array(array,*args,&block)
-    options = args.extract_options!
-    options.reverse_merge!(
-      :map => nil
-    )
-    items = args.inject([]) do |items,index|
-      item = array[index]
-      if options[:map]
-        [options[:map]].flatten.each do |method|
-          item = item.send(method) 
-        end
-      end
-      items << item
-      items
+  def skip?(*args)
+    (self.skip_if || FALSE_PROC).call(*args)
+  end
+  
+  def highlight?(*args)
+    (self.highlight_if || FALSE_PROC).call(*args)
+  end
+  
+  def compare_items(*args)
+    (self.display || DISPLAY_PROC).call(*args)
+  end
+  
+  def compare
+    items = to_compare
+    map.each do |method|
+      items = items.map(&method)
     end
-    comp(items[0],items[1],options,&block)
+    max_count = items.map(&:size).max
+    results = []
+    max_count.times do |index|
+      this_row = [index]
+      items.size.times do |column|
+        this_row << items[column][index]
+      end
+      results << compare_items(*this_row)
+    end
+    results
+  end
+  
+  def compare_string
+    compare.each do |compare_result|
+      puts compare_result
+    end
+    nil
   end
   
 end
