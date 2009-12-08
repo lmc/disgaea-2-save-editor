638d0864afce0ff44ee0aecda61fe984e1f3a76f
Luke Mcildoon <luke@twofiftyfive.net>
2009-12-08 23:38:26 +1100

    hell yes, we have working assembly http://img32.imageshack.us/img32/8451/haxz.png still to do: names are being disassembled/reassembled badly w/r/t special characters and numbers

--- config/initializers/script_console_helpers.rb
+++ config/initializers/script_console_helpers.rb
@@ -8,6 +8,6 @@ end
 
 def write_save(save_object,slot = 5)
   file = File.open("/Volumes/BONERTOWN/PSP/SAVEPLAIN/ULUS10461DISGAEA00#{slot}/SDDATA.BIN",'w') do |f|
-    
+    save_object.structures.assemble(f)
   end
 end
