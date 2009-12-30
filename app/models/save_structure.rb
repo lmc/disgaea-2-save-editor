00d9cbc51c5a2afa8fb05f85ad1b11d9afd6a4d5
Luke Mcildoon <luke@twofiftyfive.net>
2009-12-30 13:31:19 +1100

    unknown02 isn't used, but is the perfect size to bring the total map count up to 256, sooo...

--- app/models/save_structure.rb
+++ app/models/save_structure.rb
@@ -7,9 +7,7 @@ class SaveStructure < BaseData
     [:characters,       [Character,128]], #3072 x 128
     #[:unknown02,        [:unknown,7040]],
     [:senators,         [Senator,64]],
-    [:maps,             [Map,219]],
-    [:unknown02,        [:unknown,592]], #unlocked maps: ends at ~1750x2
-    #senators seem to start right here
+    [:maps,             [Map,256]],
     [:wtf_items,        [Item,4]], #wtf, there's 4 mystery items at the start of the array here
     [:pack_items,       [Item,24]],
     [:warehouse_items,  [Item,512]],
