0aef729f9e9c27d0d0cef8a57bf56966b881681d
Luke Mcildoon <luke@twofiftyfive.net>
2010-01-02 00:43:17 +1100

    more stuff found on item made a frontend for viewing unknown values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,25 @@ module ApplicationHelper
   def cs(string)
     BaseData.clean(string)
   end
+  
+  def unknown_field_for(form_builder,unknown_field_name)
+    unknown_values = form_builder.object.send(unknown_field_name).as_int8.join(', ')
+    if unknown_values.size >= 50
+      form_builder.input unknown_field_name, :as => :text, :input_html => { :value => unknown_values }
+    elsif unknown_values.size >= 15
+      form_builder.input unknown_field_name, :as => :string, :input_html => { :value => unknown_values, :class => 'long' }
+    else
+      form_builder.input unknown_field_name, :as => :string, :input_html => { :value => unknown_values }
+    end
+  end
+  
+  #FIXME: Duplicate fields header
+  def unknown_fields_for(form_builder)
+    unknown_fields = form_builder.object.class.struct_order.select { |struct_name| struct_name.to_s =~ /^unknown/ }
+    output = ""
+    unknown_fields.each do |unknown_field|
+      output << unknown_field_for(form_builder,unknown_field)
+    end
+    output
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,29 +8,32 @@ class Item < BaseData
   
   structure(
     [:specialists,          [Specialist,8]],
-    [:unknown01,            [:unknown,8]],
+    [:unknown01,            [:unknown,8]], #3,4 = lover spawn counter?
     [:stats,                Stats],
     [:base_stats,           Stats],
     [:item_class_id,        :int16],
-    [:unknown02,            [:unknown,2]],
+    [:unknown02,            [:unknown,1]], #only on human weapons
+    [:icon_id,              :int8], #TODO: Test and document icon ids
     [:level,                :int8], #true, effective level
     [:item_world_level_max, :int8],
-    [:unknown03,            [:unknown,2]],
+    [:unknown03,            [:unknown,2]], #all 0
     [:weapon_colour,        :int8], #actually used to control the colours (ie. legendary excalibur is red, value 3) normal = 1
-    [:unknown04,            [:unknown,3]], #maybe int8s, related to item world levels?
+    [:unknown04,            [:unknown,3]], #maybe int8s, related to item world levels? 3 = last bailiff level?
     [:item_world_level,     :int8], #how many item world levels cleared
-    [:unknown05,            [:unknown,5]],
+    [:unknown05,            [:unknown,5]], #4 = lover spawn counter? 5 = lover spawned?
     [:rarity,               :int8],
     
     #range? critical chance (fists are higher i think)?
-    [:unknown06,            [:unknown,2]],
+    [:unknown06,            [:unknown,2]], #1 = (0x2 + 1), 2=weapon type? same values as WeaponMastery,7=red monster,21=blue monster
     [:population,           :int8],
     [:mv,                   :int8],
     [:jmp,                  :int8],
-    [:unknown07,            [:unknown,1]], #icon ids? identical for all "angel's sandals", not for all swords? maybe duplicate entries?
-    [:maybe_is_weapon,      :int8],
-    [:unknown08,            [:unknown,3]],
-    [:rarity_badge,          :int8], #1=silver 'R', 2=gold 'L', 0=normal
+    [:item_rank,            :int8], #r39,r40. determines price, item world enemy level, etc.
+    [:range,                :int8],
+    [:unknown08,            [:unknown,1]],
+    [:critical_chance,      :int8], #naturally lower for guns, higher for fists, red monster
+    [:unknown11,            [:unknown,1]],
+    [:rarity_badge,         :int8], #1=silver 'R', 2=gold 'L', 0=normal
     [:unknown09,            [:unknown,18]], #seems to just be 00, maybe placeholder for DisgaeaString as below?
     #FIXME?: You can rename items in the Item World, will that make this become
     #  a DisgaeaString instead of String?
